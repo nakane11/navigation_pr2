c158755f188d11bfe8a675c468174861121e81c8
Aoi Nakane <nakane@jsk.imi.i.u-tokyo.ac.jp>
2022-11-25 11:19:38 +0900

    Disable amcl while slam is working

--- node_scripts/map_manager.py
+++ node_scripts/map_manager.py
@@ -3,6 +3,7 @@
 import rospy
 import roslaunch
 import os
+import dynamic_reconfigure.client
 
 class MapManager(object):
     
@@ -10,6 +11,7 @@ class MapManager(object):
         self.launch = roslaunch.scriptapi.ROSLaunch()
         self.launch.start()
         rospy.on_shutdown(self.handler)
+        self.conf = dynamic_reconfigure.client.Client('/amcl', timeout=5.0)
         self.process_gmapping = None
         self.process_saver = None
         self.process_server = None        
@@ -20,12 +22,16 @@ class MapManager(object):
     def handler(self):
         self.launch.stop()
 
+    def set_current_floor(self, floor):
+        self.current_floor = floor
+        rospy.set_param('/current_floor', floor)
 
     def start_make_map(self, floor):
         self.stop_map_server()
-        self.start_tf_publisher(floor)        
+        self.conf.update_configuration({"global_frame_id" : "dummy_map"})
+        self.start_tf_publisher(floor)
         self.start_gmapping(floor)
-        self.current_floor = floor        
+        self.set_current_floor(floor)
 
     def change_make_map(self, floor):
         self.start_map_saver(self.current_floor)
@@ -37,7 +43,7 @@ class MapManager(object):
         self.stop_tf_publisher()
         self.start_tf_publisher(floor)
         self.start_gmapping(floor)
-        self.current_floor = floor
+        self.set_current_floor(floor)
 
     def stop_make_map(self):
         self.start_map_saver(self.current_floor)
@@ -46,21 +52,22 @@ class MapManager(object):
         rospy.loginfo('Successfully saved {}.yaml!'.format(self.current_floor))
         self.stop_map_saver() 
         self.stop_gmapping()
+        self.conf.update_configuration({"global_frame_id" : "map"})
         self.start_map_server(self.current_floor)
 
     def change_floor(self, floor):
         self.stop_map_server()
         self.start_map_server(floor)
-        self.current_floor = floor
+        self.set_current_floor(floor)
 
-
-    # make /map/6f of /6f frame
+    # make /map of /map frame
     def start_gmapping(self, floor):
         package = 'gmapping'
         executable = 'slam_gmapping'
         name = 'slam_gmapping_{}'.format(floor)
         args='_odom_frame:=odom_combined _map_frame:={}'.format(floor)
-        remap_args = [('scan', 'base_scan'), ('map', 'map/{}'.format(floor))]
+        args='_odom_frame:=odom_combined _map_frame:=/map'
+        remap_args = [('scan', 'base_scan')]
         gmapping = roslaunch.core.Node(package, executable, args=args, remap_args=remap_args)
         self.process_gmapping = self.launch.launch(gmapping)
         print self.process_gmapping.is_alive()
@@ -70,14 +77,13 @@ class MapManager(object):
             self.process_gmapping.stop()
             print self.process_gmapping.is_alive()
 
-    # save /map/6f as 6f.yaml
+    # save /map as 6f.yaml
     def start_map_saver(self, floor):
         package = 'map_server'
         executable = 'map_saver'
         name = 'map_saver_{}'.format(floor)
         args='-f /tmp/raw_maps/{}'.format(floor)
-        remap_args = [('map', 'map/{}'.format(floor))]
-        saver = roslaunch.core.Node(package, executable, name=name, args=args, remap_args=remap_args)
+        saver = roslaunch.core.Node(package, executable, name=name, args=args)
         self.process_saver = self.launch.launch(saver)
         print self.process_saver.is_alive()
 
