483bee32dcddfa66144cf0acd026a3d4ecda27de
Aoi Nakane <nakane@jsk.imi.i.u-tokyo.ac.jp>
2022-11-30 16:31:01 +0900

    Update adding apot and publishing markers

--- node_scripts/spot_map_server.py
+++ node_scripts/spot_map_server.py
@@ -1,45 +1,24 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import networkx as nx
 import rospy
 import tf
 import actionlib
 import math
-import numpy as np
-import PyKDL
-import tf_conversions.posemath as pm
+from navigation_pr2.utils import *
 from std_srvs.srv import Empty, EmptyResponse
 from geometry_msgs.msg import Pose
-from visualization_msgs.msg import MarkerArray
-from navigation_pr2.msg import RecordSpotAction
+from visualization_msgs.msg import MarkerArray, Marker
+from navigation_pr2.msg import RecordSpotAction, RecordSpotResult
 from navigation_pr2.srv import ChangeFloor, ChangeFloorResponse
 from navigation_pr2.srv import Path, PathResponse
-
-def clockwise_angle_between_vectors(v1, v2, normal_vector):
-    det = np.dot(normal_vector, np.cross(v1, v2))
-    dot = np.dot(v1, v2)
-    angle = np.arctan2(-det, -dot) + np.pi
-    if np.isclose(angle, 2 * np.pi):
-        angle = 0.0
-    return angle
-
-def compute_angle_between_poses(p1, p2):
-        v1 = pm.fromMsg(p1).M * PyKDL.Vector(1,0,0)
-        v1 = np.array([v1.x(), v1.y(), v1.z()])
-        v2 = pm.fromMsg(p2).M * PyKDL.Vector(1,0,0)
-        v2 = np.array([v2.x(), v2.y(), v2.z()])
-        return clockwise_angle_between_vectors(v1, v2, [0,0,1])
-
-def compute_difference_between_poses(p1, p2):
-    diff = np.array([p1.position.x - p2.position.x,
-                     p1.position.y - p2.position.y])
-    return np.linalg.norm(diff)
 
 
 class SpotMapServer(object):
     def __init__(self):
         self.graph_dict = {}
-        self.label = 0
+        self.label = 1
         self.current_node = None
         self.auto_map_enabled = False
         self.active_graph = nx.MultiGraph()
@@ -54,7 +33,7 @@ class SpotMapServer(object):
         self.find_path = rospy.Service('~find_path', Path, self.find_path_cb)
         self.add = actionlib.SimpleActionServer('~add', RecordSpotAction, execute_cb=self.add_spot_cb)
         self.add.start()
-        rospy.Timer(rospy.Duration(0.5), self.auto_map_cb)
+        rospy.Timer(rospy.Duration(0.5), self.timer_cb)
 
     def start_auto_map(self, req):
         self.auto_map_enabled = True
@@ -68,25 +47,39 @@ class SpotMapServer(object):
 
     def timer_cb(self, event):
         if self.auto_map_enabled:
-            if not self.current_node:
-                self.add_spot(pose)
-                return
             curr_pose = self.get_robotpose()
             if not curr_pose:
+                return
+            # 前のノードがなかった場合に追加
+            if not self.current_node:
+                self.add_spot(curr_pose)
                 return
             prev_pose = self.active_graph.nodes[self.current_node]['pose']
             angle = math.degrees(compute_angle_between_poses(curr_pose, prev_pose))
             diff = compute_difference_between_poses(curr_pose, prev_pose)
+            # 前のノードとposeが十分違う場合に追加
             if ((40 < angle < 320) and diff > 0.3) or diff > 0.8:
-                self.add_spot(pose)
+                self.add_spot(curr_pose)
 
         self.publish_markers()
 
-    def publish_markers(self):
+    def publish_markers(self, action=0):
         node_array_msg = MarkerArray()
+        id = 1
+        for i in list(self.active_graph.nodes):
+            node = self.active_graph.nodes[i]
+            pin = make_pin_marker(i, node['pose'], id, rospy.Time.now(), radius=0.27)
+            node_array_msg.markers.extend(pin)
+            id += 1
+        id = 1
         edge_array_msg = MarkerArray()
+        for edge in list(self.active_graph.edges):
+            p1 = self.active_graph.nodes[edge[0]]['pose']
+            p2 = self.active_graph.nodes[edge[1]]['pose']
+            l = make_line_marker(p1, p2, id, rospy.Time.now(), color=(0,0,1))
+            edge_array_msg.markers.append(l)
         self.node_pub.publish(node_array_msg)
-        self.edge_pub.publish(edge_array_msg)        
+        self.edge_pub.publish(edge_array_msg)
     
     def add_spot_cb(self, goal):
         if goal.command == 0:
@@ -106,6 +99,7 @@ class SpotMapServer(object):
 
     def change_floor_cb(self, req):
         self.change_graph(self, req.floor)
+        self.publish_markers(action=2)
         resp = ChangeFloorResponse()
         return resp
 
@@ -117,7 +111,7 @@ class SpotMapServer(object):
         try:
             (trans,rot) = self.listener.lookupTransform('/map', '/base_link', rospy.Time(0))
         except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
-            return
+            return False
         pose = Pose()
         pose.position.x = trans[0]
         pose.position.y = trans[1]
@@ -129,42 +123,67 @@ class SpotMapServer(object):
         return pose
         
     def add_spot(self, pose, name=None):
+        # 一つ前のノードが自動記録されていて距離が近い場合に置き換え
+        if self.current_node and name:
+            prev_node = self.active_graph.nodes[self.current_node]
+            if not 'name' in prev_node:
+                prev_pose = prev_node['pose']
+                diff = compute_difference_between_poses(pose, prev_pose)
+                if diff < 0.25:
+                    node_name = name[0]
+                    mapping = {self.current_node: node_name}
+                    self.active_graph = nx.relabel_nodes(self.active_graph, mapping)
+                    self.active_graph.nodes[node_name]['name'] = name[1]
+                    self.current_node = node_name
+                    return
+        for i in list(self.active_graph.nodes):
+            node_i = self.active_graph.nodes[i]
+            if not 'name' in node_i:
+                pose_i = node_i['pose']
+                diff = compute_difference_between_poses(pose, pose_i)
+                if diff < 0.4:
+                    if self.current_node:
+                        self.active_graph.add_edge(self.current_node, i)
+                        self.current_node = i
+                    if name:
+                        node_name = name[0]
+                        mapping = {i: node_name}
+                        self.active_graph = nx.relabel_nodes(self.active_graph, mapping)
+                        self.active_graph.nodes[node_name]['name'] = name[1]
+                        self.current_node = node_name
+                    return
+        # 名前がない場合に数字を使用
         if name is None:
-            node_name = self.label
-            label += 1
+            node_name = str(self.label)
+            self.label += 1
         else:
             node_name = name[0]
-
-        if self.current_node and and name:
-            if not 'name' in self.active_graph.nodes[self.current_node]:
-                self.active_graph.nodes[self.current_node]['pose'] = pose
-                self.active_graph.nodes[self.current_node]['name'] = name
-        for n in list(self.active_graph.nodes):
-            node = self.active_graph.nodes[n]
-            if not node['name']
-            
-            
         self.active_graph.add_node(node_name)
         self.active_graph.nodes[node_name]['pose'] = pose
-        self.active_graph.nodes[node_name]['name'] = name[1]
+        if name:
+            self.active_graph.nodes[node_name]['name'] = name[1]
         # range, action, description
-        return node_name
+        if self.current_node:
+            self.active_graph.add_edge(node_name, self.current_node)
+        self.current_node = node_name
+        return
 
     def remove_spot(self, name=None, graph_name=None):
         if not name:
             self.active_graph.remove_node(self.current_node)
+            self.current_node = None
             return
         if graph_name:
             target_graph = self.graph_dict[graph_name]
             for n in list(target_graph.nodes):
-                if self.taret_graph.nodes[n]['name'] == name:
-                    self.target_graph.remove_node(n)
+                if target_graph.nodes[n]['name'] == name:
+                    target_graph.remove_node(n)
             self.graph_dict[graph_name] = target_graph
         else:
             target_graph = self.active_graph
             for n in list(target_graph.nodes):
-                if self.taret_graph.nodes[n]['name'] == name:
-                    self.target_graph.remove_node(n)
+                if target_graph.nodes[n]['name'] == name:
+                    target_graph.remove_node(n)
             self.active_graph = target_graph
         return
         
@@ -176,9 +195,7 @@ class SpotMapServer(object):
             self.active_graph = nx.MultiGraph()
         self.active_graph_name = name
 
-    def publish_marker(self):
-        return
-    
+
 if __name__ == '__main__':
     rospy.init_node('spot_map_server')
     s = SpotMapServer()
--- src/navigation_pr2/utils.py
+++ src/navigation_pr2/utils.py
@@ -1,6 +1,18 @@
 #!/usr/bin/env python
 
 import rospy
+import numpy as np
+import math
+import tf
+import PyKDL
+import tf_conversions.posemath as pm
+from std_msgs.msg import Header
+from visualization_msgs.msg import Marker
+from jsk_recognition_utils.color import labelcolormap
+from geometry_msgs.msg import Point
+
+N = 256
+colors = labelcolormap(N=N) / 255.0
 
 def wait_for_speech(timeout=0):
     start_time = rospy.Time.now()
@@ -10,3 +22,169 @@ def wait_for_speech(timeout=0):
                 return False
         continue
     return True
+
+def clockwise_angle_between_vectors(v1, v2, normal_vector):
+    det = np.dot(normal_vector, np.cross(v1, v2))
+    dot = np.dot(v1, v2)
+    angle = np.arctan2(-det, -dot) + np.pi
+    if np.isclose(angle, 2 * np.pi):
+        angle = 0.0
+    return angle
+
+def compute_angle_between_poses(p1, p2):
+        v1 = pm.fromMsg(p1).M * PyKDL.Vector(1,0,0)
+        v1 = np.array([v1.x(), v1.y(), v1.z()])
+        v2 = pm.fromMsg(p2).M * PyKDL.Vector(1,0,0)
+        v2 = np.array([v2.x(), v2.y(), v2.z()])
+        return clockwise_angle_between_vectors(v1, v2, [0,0,1])
+
+def compute_difference_between_poses(p1, p2):
+    diff = np.array([p1.position.x - p2.position.x,
+                     p1.position.y - p2.position.y])
+    return np.linalg.norm(diff)
+
+def make_cylinder(radius=0.1,
+                  height=1.0,
+                  pos=[0.0, 0.0, 0.0],
+                  q_xyzw=[0.0, 0.0, 0.0, 1.0],
+                  color=(0.0, 0.0, 0.0, 1.0),
+                  lifetime=0.25,
+                  id=0,
+                  ns='',
+                  frame_id='',
+                  stamp=None):
+    header = Header()
+    header.frame_id = frame_id
+    header.stamp = stamp
+
+    mesh_marker = Marker(type=Marker.CYLINDER, header=header, id=id)
+    mesh_marker.ns = ns
+    mesh_marker.pose.position.x = pos[0]
+    mesh_marker.pose.position.y = pos[1]
+    mesh_marker.pose.position.z = pos[2]
+    mesh_marker.pose.orientation.x = q_xyzw[0]
+    mesh_marker.pose.orientation.y = q_xyzw[1]
+    mesh_marker.pose.orientation.z = q_xyzw[2]
+    mesh_marker.pose.orientation.w = q_xyzw[3]
+    mesh_marker.scale.x = radius
+    mesh_marker.scale.y = radius
+    mesh_marker.scale.z = height
+    mesh_marker.color.b, mesh_marker.color.g, mesh_marker.color.r, \
+        mesh_marker.color.a = color
+    mesh_marker.lifetime = rospy.Duration(lifetime)
+    return mesh_marker
+
+def make_cube(x=0.1, y=0.1, z=0.1,
+              pos=[0.0, 0.0, 0.0],
+              q_xyzw=[0.0, 0.0, 0.0, 1.0],
+              color=(0.0, 0.0, 0.0, 1.0),
+              lifetime=0.25,
+              id=0,
+              ns='',
+              frame_id='',
+              stamp=None):
+    header = Header()
+    header.frame_id = frame_id
+    header.stamp = stamp
+
+    mesh_marker = Marker(type=Marker.CUBE, header=header, id=id)
+    mesh_marker.ns = ns
+    mesh_marker.pose.position.x = pos[0]
+    mesh_marker.pose.position.y = pos[1]
+    mesh_marker.pose.position.z = pos[2]
+    mesh_marker.pose.orientation.x = q_xyzw[0]
+    mesh_marker.pose.orientation.y = q_xyzw[1]
+    mesh_marker.pose.orientation.z = q_xyzw[2]
+    mesh_marker.pose.orientation.w = q_xyzw[3]
+    mesh_marker.scale.x = x
+    mesh_marker.scale.y = y
+    mesh_marker.scale.z = z
+    mesh_marker.color.b, mesh_marker.color.g, mesh_marker.color.r, \
+        mesh_marker.color.a = color
+    mesh_marker.lifetime = rospy.Duration(lifetime)
+    return mesh_marker
+
+def make_text(string,
+              scale=0.16,
+              pos=[0.0, 0.0, 0.0],
+              q_xyzw=[0.0, 0.0, 0.0, 1.0],
+              color=(0.0, 0.0, 0.0, 1.0),
+              lifetime=0.25,
+              id=0,
+              ns='',
+              frame_id='',
+              stamp=None):
+    header = Header()
+    header.frame_id = frame_id
+    header.stamp = stamp
+
+    mesh_marker = Marker(type=Marker.TEXT_VIEW_FACING, header=header, id=id)
+    mesh_marker.ns = ns
+    mesh_marker.pose.position.x = pos[0]
+    mesh_marker.pose.position.y = pos[1]
+    mesh_marker.pose.position.z = pos[2]
+    mesh_marker.pose.orientation.x = q_xyzw[0]
+    mesh_marker.pose.orientation.y = q_xyzw[1]
+    mesh_marker.pose.orientation.z = q_xyzw[2]
+    mesh_marker.pose.orientation.w = q_xyzw[3]
+    mesh_marker.scale.x = scale
+    mesh_marker.scale.y = scale
+    mesh_marker.scale.z = scale
+    mesh_marker.text = string
+    mesh_marker.color.b, mesh_marker.color.g, mesh_marker.color.r, \
+        mesh_marker.color.a = color
+    mesh_marker.lifetime = rospy.Duration(lifetime)
+    return mesh_marker
+
+def make_line(pose1=[0.0, 0.0, 0.0],
+              pose2=[0.0, 0.0, 0.0],
+              scale=0.01,
+              color=(0.0, 0.0, 0.0, 1.0),
+              lifetime=25,
+              id=0,
+              ns='',
+              frame_id='',
+              stamp=None):
+    header = Header()
+    header.frame_id = frame_id
+    header.stamp = stamp
+
+    mesh_marker = Marker(type=Marker.LINE_LIST, header=header, id=id)
+    mesh_marker.ns = ns
+    p1 = Point(x=pose1[0], y=pose1[1], z=pose1[2])
+    p2 = Point(x=pose2[0], y=pose2[1], z=pose2[2])
+    mesh_marker.points.append(p1)
+    mesh_marker.points.append(p2)
+    mesh_marker.pose.orientation.w = 1
+    mesh_marker.scale.x = scale
+    mesh_marker.color.b, mesh_marker.color.g, mesh_marker.color.r, \
+        mesh_marker.color.a = color
+    mesh_marker.lifetime = rospy.Duration(lifetime)
+    return mesh_marker
+
+def make_pin_marker(name, pose, id, stamp, height = 1.8, radius= 0.1, color=None, alpha=0.7, lifetime=15, frame_id='/map'):
+    if color is None:
+        color = colors[id % N]
+    cyl = make_cylinder(radius = radius, height = radius * 0.3,
+                        pos = [pose.position.x, pose.position.y, height/3],
+                        q_xyzw = list(tf.transformations.quaternion_from_euler(0, -math.pi/2, 0)),
+                        color = tuple(color) + (alpha,),
+                        lifetime = lifetime, id = id, ns='pin_head',frame_id = frame_id, stamp=stamp)
+    cub = make_cube(x = radius/6, y = radius/6, z = height/2.5,
+                    pos = [pose.position.x, pose.position.y, height/6],
+                    color = tuple(color) + (alpha,),
+                    lifetime = lifetime, id = id, ns='pin_body', frame_id = frame_id, stamp=stamp)
+    txt = make_text(string=name,
+                    pos = [pose.position.x, pose.position.y, height/6],
+                    color = (1, 1, 1) + (1.0,),
+                    lifetime = lifetime, id = id, ns='pin_label',frame_id = frame_id, stamp=stamp)
+
+    return [cyl, cub, txt]
+
+def make_line_marker(pose1, pose2, id, stamp, color=None, alpha=0.7, lifetime=15, frame_id='/map'):
+    if color is None:
+        color = colors[id % N]
+    line = make_line([pose1.position.x, pose1.position.y, pose1.position.z],
+                     [pose2.position.x, pose2.position.y, pose2.position.z],
+                     id, color=tuple(color) + (alpha,), frame_id=frame_id, stamp=stamp)
+    return line
