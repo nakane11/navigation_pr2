713eaa2c9df24d4bc5590ee298d99d43b0003ec9
Aoi Nakane <nakane@jsk.imi.i.u-tokyo.ac.jp>
2023-03-15 22:49:03 +0900

    Play wav when network connected/disconnected

--- node_scripts/ping.py
+++ node_scripts/ping.py
@@ -4,6 +4,11 @@ import os
 import rospy
 from std_msgs.msg import Bool
 
+from sound_play.libsoundplay import SoundClient
+
+connect_sound = "/home/nakane/nakane_ws/src/navigation_pr2/sound/connect.wav"
+disconnect_sound = "/home/nakane/nakane_ws/src/navigation_pr2/sound/disconnect.wav"
+
 class Ping():
     def __init__(self):
         self.tries = rospy.get_param('~tries', 1)
@@ -11,6 +16,9 @@ class Ping():
         self.host = rospy.get_param('~host', '8.8.8.8')
         self.pub = rospy.Publisher('/ping', Bool, queue_size=1)
         self.timer = rospy.Timer(rospy.Duration(1.0), self.timer_cb)
+        self.trigger = True
+        self.sound = SoundClient(sound_action='robotsound', blocking=False)
+        self.sound.actionclient.wait_for_server()
         
     def check(self, host):
         response = os.system("ping -c {} -w {} -q {}".format(self.tries, self.timeout, host))
@@ -19,8 +27,17 @@ class Ping():
     def timer_cb(self, event):
         res = self.check(self.host)
         if res == 0:
+            if self.trigger == True:
+                rospy.loginfo('network connected')
+                print(connect_sound)
+                self.sound.playWave(connect_sound, replace=False)
+                self.trigger = False
             self.pub.publish(Bool(data=True))
         else:
+            if self.trigger == False:
+                rospy.loginfo('network disconnected')
+                self.sound.playWave(disconnect_sound, replace=False)
+                self.trigger = True
             self.pub.publish(Bool(data=False))
 
 if __name__ == '__main__':
