b7381eaec62ee2d24da6789e671cda74ecbfa682
Aoi Nakane <nakane@jsk.imi.i.u-tokyo.ac.jp>
2023-03-22 16:18:20 +0900

    update blacklist

--- node_scripts/filter_vosk.py
+++ node_scripts/filter_vosk.py
@@ -10,21 +10,22 @@ class FilterVosk(object):
         self.pub_stamped = rospy.Publisher("/Tablet/voice_stamped/vosk_filtered", SpeechRecognitionCandidatesStamped, queue_size=1)
         self.sub = rospy.Subscriber("/Tablet/voice/vosk", SpeechRecognitionCandidates, self.cb, queue_size=1)
         self.sub_stamped = rospy.Subscriber("/Tablet/voice_stamped/vosk", SpeechRecognitionCandidatesStamped, self.cb_stamped, queue_size=1)
-        self.black_list = ["あー", "えー", "ん", "あ", "うん", "えーん", "んん"]
+        self.black_list = ["あー", "えー", "ん", "あ", "うん", "えーん", "んん", "", "ああ", "う", "なぁ"]
         
     def cb(self, msg):
-        for word in self.black_list:
-            if msg.transcript[0] == word: return
         msg.transcript[0] = msg.transcript[0].replace(' ', '')
         msg.transcript[0] = msg.transcript[0].strip('ん')
+        for word in self.black_list:
+            if msg.transcript[0] == word: return
         self.pub.publish(msg) 
 
     def cb_stamped(self, msg):
-        for word in self.black_list:
-            if msg.candidates.transcript[0] == word: return
         print(type(msg.candidates.transcript[0]))
         text = msg.candidates.transcript[0].replace(' ', '')
         text = text.strip('ん')
+        text = text.strip('あー')
+        for word in self.black_list:
+            if text == word: return
         msg.candidates.transcript[0] = text
         self.pub_stamped.publish(msg)        
         return
